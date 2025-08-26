9caf5aa423398816ab0aaff0cad9232a0892093e
Ahmed <Ahmed_Salaah@hotmail.com>
2025-08-26 11:41:26 +0300

    Initial Commit

---
+++ YassirTaskTests/YassirTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  YassirTaskTests.swift
+//  YassirTaskTests
+//
+//  Created by Ahmed Salaheldin on 26/08/2025.
+//
+
+import Testing
+@testable import YassirTask
+
+struct YassirTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
